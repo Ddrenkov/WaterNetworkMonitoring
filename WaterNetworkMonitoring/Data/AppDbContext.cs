04c70ab9e98a284fce6b0da7b1846602853a6197
ddrenkov <ddrenkov@abv.bg>
2026-09-25 23:06:06 +0300

    Initial Windows Forms project setup

---
+++ WaterNetworkMonitoring/Data/AppDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using WaterNetworkMonitoring.Models;
+
+namespace WaterNetworkMonitoring.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Account> Accounts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Server=DenislavPC\MSSQLSERVER01;
+                  Database=WaterNetworkDB;
+                  Trusted_Connection=True;
+                  TrustServerCertificate=True;"
+            );
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Account>()
+                .HasIndex(a => a.Username)
+                .IsUnique();
+
+            modelBuilder.Entity<Account>()
+                .HasIndex(a => a.Email)
+                .IsUnique();
+        }
+    }
+}
